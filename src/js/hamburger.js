837795ee7f48e2dd3783dbfe6a42abcbe3654513
OrlandoMendoza17 <ommv.17@gmail.com>
2020-09-11 01:05:32 -0400

    creating footer

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,29 +1,46 @@
-const $menuItems = document.getElementsByClassName('menu-items')[0]
-const $menuOverlay = document.getElementsByClassName('menu-overlay')[0]
-const $user = document.getElementsByClassName('user')[0]
-const $userPanel = $user.getElementsByTagName('ul')[0]
+// Main Menu Elements
 const $hamburger = document.getElementById('hamburger')
+const $menuOverlay = document.getElementsByClassName('menu-overlay')[0]
+const $menuItems = document.getElementsByClassName('menu-items')[0]
+// Menu User Panel
+// const $user = document.getElementsByClassName('user')[0]
+// const $userPanel = $user.getElementsByTagName('ul')[0]
+// const $userArrow = $user.getElementsByClassName('fa-angle-down')[0]
 
-$hamburger.addEventListener('click', ()=>{
-  if($menuItems.classList.contains('hide')){
+class Menu{
+  constructor(displayButton, menuList, optionalElement, class1, class2){
+    this.boton = displayButton;
+    this.menu = menuList;
+    this.option = optionalElement;
+    this.clase1 = class1;
+    this.clase2 = class2;
+  }
+}
 
-    $menuItems.classList.remove('hide')
-    $menuOverlay.classList.remove('hide')
+const displayMenu = (menuType) => {
+  menuType.boton.addEventListener('click', () => {
+    menuType.menu.classList.toggle(menuType.clase1)
+    menuType.option.classList.toggle(menuType.clase2)
+  })
+}
+// document.addEventListener('DOMContentLoaded', (event) =>{
+  
+//   if($userPanel.children[0].children[0].innerText === ""){
+//     //Hay sesión iniciada
+//     $user.classList.toggle('d-none')
     
-  }else if(!$menuItems.classList.contains('hide')){
-    
-    $menuItems.classList.add('hide')
-    $menuOverlay.classList.add('hide')
-  }
-})
-$menuOverlay.addEventListener('click', ()=>{
-  if(!$menuItems.classList.contains('hide')){
-    
-    $menuItems.classList.add('hide')
-    $menuOverlay.classList.add('hide')
-  }
-})
-$user.addEventListener('click', ()=>{
-  $userPanel.classList.toggle('hide')
-  $user.getElementsByClassName('fa-angle-down')[0].classList.toggle('rotate')
-})
+//   }else{
+//     //No hay sesión iniciada
+//     $menuItems.classList.toggle('four-buttons')
+//     $menuItems.children[3].classList.toggle('d-none')
+//   }
+// })
+
+const menuHamburger = new Menu($hamburger, $menuItems, $menuOverlay, 'hide', 'hide')
+const menuOverlay = new Menu($menuOverlay, $menuItems, $menuOverlay, 'hide', 'hide')
+// const menuUser = new Menu($user, $userPanel, $userArrow, 'hide', 'rotate')
+
+// const menu = [menuHamburger, menuOverlay, menuUser]
+const menu = [menuHamburger, menuOverlay]
+
+menu.forEach(menuType => displayMenu(menuType))
