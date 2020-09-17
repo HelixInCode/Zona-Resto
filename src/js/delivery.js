b133ad2886db93808481958dccad71f528394766
OrlandoMendoza17 <ommv.17@gmail.com>
2020-09-17 14:12:54 -0400

    Creacion de pedido a punto

---
+++ src/js/delivery.js
@@ -0,0 +1,19 @@
+const $inputContainer = document.querySelector('#factura > .factura-container > .input-container');
+const $inputForms = document.querySelector('#info-cliente > .info-cliente-container > .formulario');
+
+const cambiarForm = ()=>{
+  $inputForms.classList.toggle('delivery')
+  $inputForms.children[3].classList.toggle('d-none')
+}
+
+$inputContainer.addEventListener('click', (event)=>{
+  
+  if(event.target.id === 'delivery'){
+   
+    cambiarForm()
+
+  }else if(event.target.id === 'retiro'){
+    
+    cambiarForm()
+  }
+})
