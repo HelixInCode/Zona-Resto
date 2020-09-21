f1359259c41b368c151ff0e0e717e1a49522da0c
Lucas <lucasadriansidan@gmail.com>
2020-09-21 20:28:24 -0300

    conexion.php

---
+++ conexion.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "zona_resto";
+
+function conectar()
+    {global $host, $user, $pass, $db; 
+     $cnx = mysqli_connect ($host, $user, $pass, $db);
+     if (mysqli_connect_errno()){
+        echo "Conexión fallida" .mysqli_connect_error();
+        exit(); } 
+    return $cnx;}
+
+$conexion = conectar();
+    
+?>
